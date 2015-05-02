a7a8d4eb74bd95ef3eacf63944c0a7dc90ad5a84
dwinters <dwinters>
2015-05-02 12:57:16 +0000

    cleanup: remove unused functions and reformat license text

--- fit.cpp
+++ fit.cpp
@@ -1,53 +1,28 @@
 // Copyright (c) 2010,2011 Daniel Winters <daniel@tydirium.org>
 
 // Permission to use, copy, modify, and distribute this software for
-// any purpose with or without fee is hereby granted, provided that the
-// above copyright notice and this permission notice appear in all
+// any purpose with or without fee is hereby granted, provided that
+// the above copyright notice and this permission notice appear in all
 // copies.
 
 // THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
 // WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
-// AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
-// DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA
-// OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
-// TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-// PERFORMANCE OF THIS SOFTWARE.
+// AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+// CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+// OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+// NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+// CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 #include "fit.h"
 
+using namespace cv;
 
-void printmatrix(cv::Mat mat) {
-  int row, col; 
-  
-  std::cout << std::endl << std::endl;
-
-  for (row=0; row<mat.rows; row++) {
-    double* Mi = mat.ptr<double>(row);
-    for (col=0;	col<mat.cols; col++) {
-      std::cout << Mi[col] << " ";
-    }
-    std::cout << std::endl;
-  }
-
-  std::cout << std::endl << std::endl;
-}
-
-void printarray(double *array, int numofpoints) {
-  int row;
-
-  std::cout << std::endl << std::endl;
-
-  for (row=0; row<numofpoints; row++)
-    std::cout << array[row] << ", ";
-
-  std::cout << std::endl << std::endl;
-}
-  
-cv::Mat vandermonde(int degree, double *x, int numofpoints) {
+Mat vandermonde(int degree, double *x, int numofpoints)
+{
   int row, col;
 
-  cv::Mat v(numofpoints,degree+1,CV_64FC1);
+  Mat v(numofpoints,degree+1,CV_64FC1);
 
   for (row=0; row<numofpoints; row++) {
     double* Mi = v.ptr<double>(row);
@@ -59,18 +34,19 @@ cv::Mat vandermonde(int degree, double *x, int numofpoints) {
 }
 
 
-int polyfit(double *fit_results, double *xvals, double *yvals, int degree, int numofpoints) {
+int polyfit(double *fit_results, double *xvals, double *yvals, int degree, int numofpoints)
+{
   int row;
-  cv::Mat y(numofpoints,1,CV_64FC1,yvals);
+  Mat y(numofpoints,1,CV_64FC1,yvals);
   
-  cv::Mat v;
+  Mat v;
   v=vandermonde(degree,xvals,numofpoints);
 	
-  cv::Mat vtrans, vadj;
-  cv::transpose(v,vtrans);
-  cv::invert(vtrans*v,vadj);
+  Mat vtrans, vadj;
+  transpose(v,vtrans);
+  invert(vtrans*v,vadj);
 
-  cv::Mat x;
+  Mat x;
   x=vadj*vtrans*y;
 
   for (row=0;row<degree+1;row++) {
